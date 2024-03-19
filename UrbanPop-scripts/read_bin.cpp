326120507c3f7202f5640ec6c3ce8db3eda7c488
Steven Hofmeyr <shofmeyr@lbl.gov>
2024-03-19 10:24:01 -0700

    python processing script now converts urbanpop feather files to packed C structs

---
+++ UrbanPop-scripts/read_bin.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+#include "UrbanPopAgentStruct.H"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    ifstream f(argv[1], std::ios::binary);
+    int lines = 0;
+    for (;; lines++) {
+        UrbanPopAgent agent;
+        agent.read_binary(f);
+        cout << agent << endl;
+        if (f.peek() == EOF) break;
+    }
+    cout << "Read " << lines << " lines\n";
+    return 0;
+}
